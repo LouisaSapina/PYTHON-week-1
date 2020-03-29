414b795cef6488a29a31b0625022194e2e1b0ad9
Louisa Sapina <62461105+LouisaSapina@users.noreply.github.com>
2020-03-29 19:11:41 +0600

    Add files via upload

---
+++ electronnie chasi 1.py
@@ -0,0 +1,2 @@
+min = int(input())
+print(min // 60 % 24, min % 60)
---
+++ penguins.py
@@ -0,0 +1,11 @@
+number = int(input())
+p = "   _~_    "
+e = "  (o o)   "
+n = " /  V  \  "
+g = "/(  _  )\ "
+u = "  ^^ ^^   "
+print(number * p)
+print(number * e)
+print(number * n)
+print(number * g)
+print(number * u)
